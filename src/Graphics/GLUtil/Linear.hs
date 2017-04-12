a7276db223d0295ffeed790be59760ccce061db5
Jaro Reinders <noughtmare@openmailbox.org>
2017-04-12 23:10:50 +0200

    Removed hpp dependency

--- src/Graphics/GLUtil/Linear.hs
+++ src/Graphics/GLUtil/Linear.hs
@@ -6,6 +6,7 @@ module Graphics.GLUtil.Linear (AsUniform(..)) where
 import Foreign.Marshal.Array (withArray)
 import Foreign.Marshal.Utils (with)
 import Foreign.Ptr (Ptr, castPtr)
+import Foreign.Storable (Storable)
 import Graphics.Rendering.OpenGL
 import Graphics.GL.Core31
 import Linear
@@ -28,57 +29,71 @@ castVecComponent = castPtr
 castMatComponent :: Ptr (t (f a)) -> Ptr a
 castMatComponent = castPtr
 
-instance AsUniform GLint where
-  x `asUniform` loc = with x $ glUniform1iv (getUL loc) 1
+mkAsUniformScalar :: Storable a => (GLint -> GLsizei -> Ptr a -> IO b) -> a -> UniformLocation -> IO b
+mkAsUniformScalar f x loc = with x $ f (getUL loc) 1
 
-instance AsUniform GLuint where
-  x `asUniform` loc = with x $ glUniform1uiv (getUL loc) 1
+instance AsUniform GLint   where asUniform = mkAsUniformScalar glUniform1iv
+instance AsUniform GLuint  where asUniform = mkAsUniformScalar glUniform1uiv
+instance AsUniform GLfloat where asUniform = mkAsUniformScalar glUniform1fv
 
-instance AsUniform GLfloat where
-  x `asUniform` loc = with x $ glUniform1fv (getUL loc) 1
+instance AsUniform TextureUnit
+instance UniformComponent a => AsUniform (Index1 a)
+instance UniformComponent a => AsUniform (Color4 a)
+instance UniformComponent a => AsUniform (Color3 a)
+instance UniformComponent a => AsUniform (FogCoord1 a)
+instance UniformComponent a => AsUniform (Normal3 a)
+instance UniformComponent a => AsUniform (TexCoord4 a)
+instance UniformComponent a => AsUniform (TexCoord3 a)
+instance UniformComponent a => AsUniform (TexCoord2 a)
+instance UniformComponent a => AsUniform (TexCoord1 a)
+instance UniformComponent a => AsUniform (Vertex4 a)
+instance UniformComponent a => AsUniform (Vertex3 a)
+instance UniformComponent a => AsUniform (Vertex2 a)
 
-instance AsUniform TextureUnit where
-instance UniformComponent a => AsUniform (Index1 a) where
-instance UniformComponent a => AsUniform (Color4 a) where
-instance UniformComponent a => AsUniform (Color3 a) where
-instance UniformComponent a => AsUniform (FogCoord1 a) where
-instance UniformComponent a => AsUniform (Normal3 a) where
-instance UniformComponent a => AsUniform (TexCoord4 a) where
-instance UniformComponent a => AsUniform (TexCoord3 a) where
-instance UniformComponent a => AsUniform (TexCoord2 a) where
-instance UniformComponent a => AsUniform (TexCoord1 a) where
-instance UniformComponent a => AsUniform (Vertex4 a) where
-instance UniformComponent a => AsUniform (Vertex3 a) where
-instance UniformComponent a => AsUniform (Vertex2 a) where
+mkAsUniformVec :: Storable (t a) => (GLint -> GLsizei -> Ptr a -> IO b) -> t a -> UniformLocation -> IO b
+mkAsUniformVec f v loc = with v $ f (getUL loc) 1 . castVecComponent
 
-#define UNIFORMVEC_T(d,ht,glt) instance AsUniform (V ## d ht) where {v `asUniform` loc = with v $ glUniform##d##glt##v (getUL loc) 1 . castVecComponent}
+instance AsUniform (V1 GLint  ) where asUniform = mkAsUniformVec glUniform1iv
+instance AsUniform (V1 GLuint ) where asUniform = mkAsUniformVec glUniform1uiv
+instance AsUniform (V1 GLfloat) where asUniform = mkAsUniformVec glUniform1fv
 
-#define UNIFORMVEC(d) UNIFORMVEC_T(d,GLint,i); UNIFORMVEC_T(d,GLuint,ui); UNIFORMVEC_T(d,GLfloat,f)
+instance AsUniform (V2 GLint  ) where asUniform = mkAsUniformVec glUniform2iv
+instance AsUniform (V2 GLuint ) where asUniform = mkAsUniformVec glUniform2uiv
+instance AsUniform (V2 GLfloat) where asUniform = mkAsUniformVec glUniform2fv
 
-UNIFORMVEC(1)
-UNIFORMVEC(2)
-UNIFORMVEC(3)
-UNIFORMVEC(4)
+instance AsUniform (V3 GLint  ) where asUniform = mkAsUniformVec glUniform3iv
+instance AsUniform (V3 GLuint ) where asUniform = mkAsUniformVec glUniform3uiv
+instance AsUniform (V3 GLfloat) where asUniform = mkAsUniformVec glUniform3fv
 
-instance AsUniform (M22 GLfloat) where
-  m `asUniform` loc = with m
-                    $ glUniformMatrix2fv (getUL loc) 1 1 . castMatComponent
+instance AsUniform (V4 GLint  ) where asUniform = mkAsUniformVec glUniform4iv
+instance AsUniform (V4 GLuint ) where asUniform = mkAsUniformVec glUniform4uiv
+instance AsUniform (V4 GLfloat) where asUniform = mkAsUniformVec glUniform4fv
 
-instance AsUniform (M33 GLfloat) where
-  m `asUniform` loc = with m
-                    $ glUniformMatrix3fv (getUL loc) 1 1 . castMatComponent
+mkAsUniformMat :: Storable (t (f a)) => (GLint -> GLsizei -> GLboolean -> Ptr a -> IO b) -> t (f a) -> UniformLocation -> IO b
+mkAsUniformMat f m loc = with m $ f (getUL loc) 1 1 . castMatComponent
 
-instance AsUniform (M44 GLfloat) where
-  m `asUniform` loc = with m
-                    $ glUniformMatrix4fv (getUL loc) 1 1 . castMatComponent
+instance AsUniform (M22 GLfloat) where asUniform = mkAsUniformMat glUniformMatrix2fv
+instance AsUniform (M33 GLfloat) where asUniform = mkAsUniformMat glUniformMatrix3fv
+instance AsUniform (M44 GLfloat) where asUniform = mkAsUniformMat glUniformMatrix4fv
 
 -- Support lists of vectors as uniform arrays of vectors.
 
-#define UNIFORMARRAY_T(d,ht,glt) instance AsUniform [V##d ht] where {l `asUniform` loc = withArray l $ glUniform##d##glt##v (getUL loc) (fromIntegral $ length l) . castVecComponent}
+mkAsUniformList :: Storable (t a) => (GLint -> GLsizei -> Ptr a -> IO b) -> [t a] -> UniformLocation -> IO b
+mkAsUniformList f l loc = withArray l $ f (getUL loc) (fromIntegral $ length l) . castVecComponent
 
-#define UNIFORMARRAY(d) UNIFORMARRAY_T(d,GLint,i); UNIFORMARRAY_T(d,GLuint,ui); UNIFORMARRAY_T(d,GLfloat,f)
+instance AsUniform [V1 GLint  ] where asUniform = mkAsUniformList glUniform1iv
+instance AsUniform [V1 GLuint ] where asUniform = mkAsUniformList glUniform1uiv
+instance AsUniform [V1 GLfloat] where asUniform = mkAsUniformList glUniform1fv
 
-UNIFORMARRAY(1)
-UNIFORMARRAY(2)
-UNIFORMARRAY(3)
-UNIFORMARRAY(4)
+instance AsUniform [V2 GLint  ] where asUniform = mkAsUniformList glUniform2iv
+instance AsUniform [V2 GLuint ] where asUniform = mkAsUniformList glUniform2uiv
+instance AsUniform [V2 GLfloat] where asUniform = mkAsUniformList glUniform2fv
+
+instance AsUniform [V3 GLint  ] where asUniform = mkAsUniformList glUniform3iv
+instance AsUniform [V3 GLuint ] where asUniform = mkAsUniformList glUniform3uiv
+instance AsUniform [V3 GLfloat] where asUniform = mkAsUniformList glUniform3fv
+
+instance AsUniform [V4 GLint  ] where asUniform = mkAsUniformList glUniform4iv
+instance AsUniform [V4 GLuint ] where asUniform = mkAsUniformList glUniform4uiv
+instance AsUniform [V4 GLfloat] where asUniform = mkAsUniformList glUniform4fv
+
